80e18fb059ea3ba394a2f9f71f6b85d66e1f76c8
andarino <tj.vinci@gmail.com>
2023-03-09 20:36:22 -0300

    first commit

---
+++ src/main/java/me/dio/academia/digital/entity/Aluno.java
@@ -0,0 +1,32 @@
+package me.dio.academia.digital.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_alunos")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"}) //por causa do lazy
+public class Aluno {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    @Column(unique = true)
+    private String cpf;
+    private String bairro;
+    private LocalDate dataDeNascimento;
+
+    @OneToMany(mappedBy = "aluno", fetch = FetchType.LAZY) //vai retornar todas as avaliacoes fisicas menos "camila"
+    @JsonIgnore //para evitar um loop de avaliacoes
+    private List<AvaliacaoFisica> avaliacoes = new ArrayList<>();
+}
